295bc561288c5fd002ea13be8d456fff6e2d3801
zhangjian <zhangjian@fhcard.com>
2016-03-03 18:31:53 +0800

    init

---
+++ weixin-service/src/main/java/cn/fhcard/weixin/service/App.java
@@ -0,0 +1,13 @@
+package cn.fhcard.weixin.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
